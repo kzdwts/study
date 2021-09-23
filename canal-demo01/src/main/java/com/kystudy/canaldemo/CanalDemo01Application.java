03ab665ba9d8bdb5a8c8967dfcd86ea7575e698b
kangyong <kangyong_wts@sina.com>
2021-09-23 22:07:15 +0800

    canal 1.1.4版本，读取binglog

---
+++ canal-demo01/src/main/java/com/kystudy/canaldemo/CanalDemo01Application.java
@@ -0,0 +1,20 @@
+package com.kystudy.canaldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TODO description.
+ *
+ * @author kangyong
+ * @date 2021/9/23
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class CanalDemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanalDemo01Application.class, args);
+    }
+
+}
