32be7c446dc49a7b9ef50e895fb750950089b7fa
kangyong <kangyong_wts@sina.com>
2022-07-01 16:07:12 +0800

    feat: test

---
+++ springboot-pagehelper-demo01/src/main/java/com/kystudy/pagehelperdemo/PageHelperDemo01Application.java
@@ -0,0 +1,19 @@
+package com.kystudy.pagehelperdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author Kang Yong
+ * @date 2021/11/5
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class PageHelperDemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PageHelperDemo01Application.class, args);
+    }
+}
